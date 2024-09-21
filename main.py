0d4c3bb169ffaa7ac94665e76ceda21debe06374
ADITYA <aditya11surve08@gmail.com>
2024-09-21 15:04:02 +0530

    day 27: api app

---
+++ main.py
@@ -0,0 +1,8 @@
+import requests
+
+api_key = "30a6f116b94b4eb9b3497ae31fa99a81"
+url = "https://newsapi.org/v2/everything?q=tesla&from=2024-08-21&sortBy=publishedAt&apiKey=30a6f116b94b4eb9b3497ae31fa99a81"
+
+request = requests.get(url)
+content = request.json()
+print(content)

549c4ff6203c0648a01597eacb60a6d05fabf742
ADITYA <aditya11surve08@gmail.com>
2024-09-21 15:36:03 +0530

    day 27: add send email option

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import requests
+from send_email import send_email
 
 api_key = "30a6f116b94b4eb9b3497ae31fa99a81"
 url = "https://newsapi.org/v2/everything?q=tesla&from=2024-08-21&sortBy=publishedAt&apiKey=30a6f116b94b4eb9b3497ae31fa99a81"
 
 request = requests.get(url)
 content = request.json()
+
+body = ""
 for article in content["articles"]:
-    print(article["title"])
-    print(article["description"])
+    if article["title"] is not None:
+        body = body + article["title"] + "\n" + article["description"] + 2*"\n"
+
+body = body.encode("utf-8")
+send_email(message=body)
